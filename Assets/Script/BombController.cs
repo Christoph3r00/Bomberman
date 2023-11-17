3a6b2be585887a36732210ea20b6b3841cd0fc06
Christoph3r00 <Yerves.christopher@gmail.com>
2023-11-16 21:50:31 -0600

    Initial commit

---
+++ Assets/Script/BombController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BombController : MonoBehaviour
+{
+    [Header("Bomb")]
+    public KeyCode inputKey = KeyCode.LeftShift;
+    public GameObject bombPrefab;
+    public float bombFuseTime = 3f;
+    public int bombAmount = 1;
+    private int bombsRemaining;
+
+    public BombController(int bombAmount)
+    {
+        this.bombAmount = bombAmount;
+    }
+
+    private void OnEnable()
+    {
+        bombsRemaining = bombAmount;
+    }
+
+    private void Update()
+    {
+        if (bombsRemaining > 0 && Input.GetKeyDown(inputKey))
+        {
+            StartCoroutine(PlaceBomb());
+        }
+    }
+
+    private IEnumerator PlaceBomb()
+    {
+        Vector2 position = transform.position;
+        position.x = Mathf.Round(position.x);
+        position.y = Mathf.Round(position.y);
+
+        GameObject bomb = Instantiate(bombPrefab, position, Quaternion.identity);
+        bombsRemaining--;
+
+        yield return new WaitForSeconds(bombFuseTime);
+        Destroy(bomb);
+
+       
+
+}
+}
